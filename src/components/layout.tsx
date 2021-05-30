6e96d4101ad57ddd4f7859dde0f71729c67d602b
omurakazuaki <omura@enrapt.jp>
2021-05-30 21:11:19 +0900

    refactor

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,104 +1,21 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql, Link } from "gatsby"
-
-import "./layout.css"
-
-import { Container, Main, Nav, Header, NavList, NavSection, NavItem } from "./styled"
+import { Container, Main, Header } from "./styled"
 import Logo from './logo';
+import Navigator from './navigator';
+import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query DocsIndex {
+    query SiteData {
       site {
         siteMetadata {
           title
         }
       }
-      allMdx(
-        filter: {
-          fileAbsolutePath: {glob: "**/docs/**"}
-        }
-      ) {
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-            }
-            fields {
-              slug
-            }
-          }
-        }
-      }
     }
   `);
-
-  const indexTree = data.allMdx.edges.reduce((tree, {node}) => {
-    const path = node.fields.slug.split('/').filter(v=>v.length > 0);
-    let current = tree;
-    while (path.length) {
-      const dir = path.shift();
-      if (path.length) {
-        if (current[dir] == undefined) {
-          current[dir] = {dir};
-        }
-      } else {
-        current[dir] = {
-          dir,
-          isLeaf: true,
-          slug: node.fields.slug,
-          title: node.frontmatter.title
-        };
-      }
-      current = current[dir];
-    }
-    return tree;
-  }, {});
-
-  const PageList = (node) => {
-    if (node.isLeaf) {
-      return (
-        <NavItem key={node.slug}>
-          <Link to={node.slug}
-            style={{
-              display: 'flex',
-              padding: 8
-            }}
-            activeStyle={{
-              color: '#663399',
-              backgroundColor: '#fff'
-            }}
-          >
-          {node.title}
-          </Link>
-        </NavItem>);
-    } else {
-      return (
-        <div key={node.dir}>
-          { node.dir ? <NavSection>{node.dir}</NavSection> : <></>}
-          <NavList>
-            {Object.keys(node)
-              .filter(key=>key != 'dir')
-              .map(key=>node[key])
-              .sort((a, b) => {
-                if (a.isLeaf && !b.isLeaf) return -1;
-                if (!a.isLeaf && b.isLeaf) return 1;
-                return a.dir.localeCompare(b.dir);
-              })
-              .map(newNode=>PageList(newNode))}
-          </NavList>
-        </div>);
-    }
-  };
 
   const [ isVisibleNav, setVisibleNav ] = useState(false);
 
@@ -110,9 +27,7 @@ const Layout = ({ children }) => {
         }} />
         <Link to="/">{data.site.siteMetadata.title}</Link>
       </Header>
-      <Nav isVisible={isVisibleNav}>
-        { PageList(indexTree) }
-      </Nav>
+      <Navigator isVisible={isVisibleNav}/>
       <Main>
         {children}
       </Main>
---
+++ src/components/navigator.tsx
@@ -0,0 +1,93 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { Nav, NavList, NavSection, NavItem } from "./styled"
+
+const Navigator = ({isVisible} : {isVisible: boolean}) => {
+  const data = useStaticQuery(graphql`
+    query DocsIndex {
+      allMdx(
+        filter: {
+          fileAbsolutePath: {glob: "**/docs/**"}
+        }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const indexTree = data.allMdx.edges.reduce((tree, {node}) => {
+    const path = node.fields.slug.split('/').filter(v=>v.length > 0);
+    let current = tree;
+    while (path.length) {
+      const dir = path.shift();
+      if (path.length) {
+        if (current[dir] == undefined) {
+          current[dir] = {dir};
+        }
+      } else {
+        current[dir] = {
+          dir,
+          isLeaf: true,
+          slug: node.fields.slug,
+          title: node.frontmatter.title
+        };
+      }
+      current = current[dir];
+    }
+    return tree;
+  }, {});
+
+  const PageList = (node) => {
+    if (node.isLeaf) {
+      return (
+        <NavItem key={node.slug}>
+          <Link to={node.slug}
+            style={{
+              display: 'flex',
+              padding: 8
+            }}
+            activeStyle={{
+              color: '#663399',
+              backgroundColor: '#fff'
+            }}
+          >
+          {node.title}
+          </Link>
+        </NavItem>);
+    } else {
+      return (
+        <div key={node.dir}>
+          { node.dir ? <NavSection>{node.dir}</NavSection> : <></>}
+          <NavList>
+            {Object.keys(node)
+              .filter(key=>key != 'dir')
+              .map(key=>node[key])
+              .sort((a, b) => {
+                if (a.isLeaf && !b.isLeaf) return -1;
+                if (!a.isLeaf && b.isLeaf) return 1;
+                return a.dir.localeCompare(b.dir);
+              })
+              .map(newNode=>PageList(newNode))}
+          </NavList>
+        </div>);
+    }
+  };
+
+  return (
+    <Nav isVisible={isVisible}>
+      { PageList(indexTree) }
+    </Nav>
+  )
+}
+
+export default Navigator;
